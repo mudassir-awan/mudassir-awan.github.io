595dddddd6041b6c1cad3bfe9a694ba2fd9991e5
Mudassir Ibrahim Awan <42530143+mudassir-awan@users.noreply.github.com>
2023-05-17 18:44:57 +0900

    jjj

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -70,7 +70,7 @@ for row, item in publications.iterrows():
     
     ## YAML variables
     
-    md = "---\ntitle: \""   + item.title + '"\n' + 'mmmmmmmmmmmmuuuuuuuuuuuuudaaaaaaaaaasss'
+    md = "---\ntitle: \""   + item.title + '"\n'
     
     md += """collection: publications"""
     
